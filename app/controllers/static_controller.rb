4b2d7df2e852cad6e39ac95b3eaf28c83bec3e50
Bearinawolfpack <bearinawolfpack@gmail.com>
2020-06-23 10:40:10 -0400

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,6 @@
+class StaticController < ApplicationController
+    def hello_world
+        render "hello_world"
+    end
+    # hello_world.html.erb
+end # end of StaticController
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # hello_world url path  # passed through the static controller's hello_world action(ruby method)
+  get 'hello_world', to: 'static#hello_world'
 end
